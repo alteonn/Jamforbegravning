ae9ab610719fe76b508e4ca98c92f475dd33a8e4
alteonn <info@flytti.se>
2024-11-28 01:36:32 +0100

    uppdaterat favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
+import Head from 'next/head'
 import '../styles/globals.css'
 import Footer from '../components/Layout/Footer'
 import CookieBanner from '../components/CookieBanner'
@@ -12,6 +13,15 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
+      <Head>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="android-chrome" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="android-chrome" sizes="512x512" href="/android-chrome-512x512.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
       <div className="min-h-screen bg-warm-50">
         <Component {...pageProps} />
         <Footer />
